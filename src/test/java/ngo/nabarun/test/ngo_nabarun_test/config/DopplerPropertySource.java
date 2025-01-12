3fd2e6376056a2ed677412401c18c6616cca8990
Souvik Sarkar <souviksarrkar362@gamil.com>
2025-01-12 11:17:04 +0530

    Merge branch 'master' of https://github.com/nabarun-ngo/ngo-nabarun-test.git

--- src/test/java/ngo/nabarun/test/ngo_nabarun_test/config/DopplerPropertySource.java
+++ src/test/java/ngo/nabarun/test/ngo_nabarun_test/config/DopplerPropertySource.java
@@ -19,54 +19,55 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import ngo.nabarun.test.ngo_nabarun_test.helpers.CommonHelpers;
 
 public class DopplerPropertySource {
-	
+
 	private String projectName;
 	private String configName;
 	private String serviceToken;
 
-	public DopplerPropertySource(String projectName,String configName,String serviceToken) {
-		this.projectName=projectName;
-		this.configName=configName.toLowerCase();
-		this.serviceToken=serviceToken;
+	public DopplerPropertySource(String projectName, String configName, String serviceToken) {
+		this.projectName = projectName;
+		this.configName = configName.toLowerCase();
+		this.serviceToken = serviceToken;
 	}
 
-	
 	public Map<String, Object> loadProperties() throws Exception {
 		Map<String, Object> propertySource = new HashMap<>();
 
 		try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
 
-            HttpGet httpget = new HttpGet("https://api.doppler.com/v3/configs/config/secrets?project="+projectName+"&config="+configName+"&include_dynamic_secrets=false&include_managed_secrets=false");
-            
-            httpget.setHeader(HttpHeaders.ACCEPT, "application/json");
-            httpget.setHeader(HttpHeaders.AUTHORIZATION, "Bearer "+serviceToken);
-            
-            ResponseHandler< String > responseHandler = response -> {
-                int status = response.getStatusLine().getStatusCode();
-                if (status >= 200 && status < 300) {
-                    HttpEntity entity = response.getEntity();
-                    return entity != null ? EntityUtils.toString(entity) : null;
-                } else {
-                    HttpEntity entity = response.getEntity();
-                    throw new ClientProtocolException("Unexpected response status: " + status+" -> "+ EntityUtils.toString(entity));
-                }
-            };
-            
-            String responseBody = httpclient.execute(httpget, responseHandler);
-            System.out.println(responseBody);
-            ObjectMapper objectMapper = CommonHelpers.objectMapper;
-            JsonNode respNode = objectMapper.readTree(responseBody);
-            JsonNode secretsNode = respNode.get("secrets");
-            Iterator<Map.Entry<String, JsonNode>> fields = secretsNode.fields();
-            System.out.println("Test Proterties:");
-            while (fields.hasNext()) {
-                Map.Entry<String, JsonNode> field = fields.next();
-                String key = field.getKey();
-                JsonNode rawValue = field.getValue().get("raw");
-                propertySource.put(key, rawValue.asText());
-                System.out.println(key+" : "+rawValue.asText());
-            }
-        }
+			HttpGet httpget = new HttpGet("https://api.doppler.com/v3/configs/config/secrets?project=" + projectName
+					+ "&config=" + configName + "&include_dynamic_secrets=false&include_managed_secrets=false");
+
+			httpget.setHeader(HttpHeaders.ACCEPT, "application/json");
+			httpget.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + serviceToken);
+
+			ResponseHandler<String> responseHandler = response -> {
+				int status = response.getStatusLine().getStatusCode();
+				if (status >= 200 && status < 300) {
+					HttpEntity entity = response.getEntity();
+					return entity != null ? EntityUtils.toString(entity) : null;
+				} else {
+					HttpEntity entity = response.getEntity();
+					throw new ClientProtocolException(
+							"Unexpected response status: " + status + " -> " + EntityUtils.toString(entity));
+				}
+			};
+
+			String responseBody = httpclient.execute(httpget, responseHandler);
+			ObjectMapper objectMapper = CommonHelpers.objectMapper;
+			JsonNode respNode = objectMapper.readTree(responseBody);
+			JsonNode secretsNode = respNode.get("secrets");
+			Iterator<Map.Entry<String, JsonNode>> fields = secretsNode.fields();
+			System.out.println("Test Proterties:");
+			while (fields.hasNext()) {
+				Map.Entry<String, JsonNode> field = fields.next();
+				String key = field.getKey();
+				String rawValue = field.getValue().get("raw").asText();
+				String rawVisibility = field.getValue().get("rawVisibility").asText();
+				propertySource.put(key, rawValue);
+				System.out.println(key + " : " + (rawVisibility.equals("restricted") ? "***" : rawValue));
+			}
+		}
 		return propertySource;
 	}
 

c1a119c272f74faefe0835edb66ab05ca2500502
Souvik Sarkar <souviksarrkar362@gamil.com>
2025-03-16 01:12:31 +0530

    test2

--- src/test/java/ngo/nabarun/test/ngo_nabarun_test/hooks/TestHooks.java
+++ src/test/java/ngo/nabarun/test/ngo_nabarun_test/hooks/TestHooks.java
@@ -4,8 +4,6 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;

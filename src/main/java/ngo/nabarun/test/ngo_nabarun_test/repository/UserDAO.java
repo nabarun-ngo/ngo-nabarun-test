adb282164f0e9a0410c2201d538cef794d36ae72
Souvik Sarkar <souviksarrkar362@gmail.com>
2026-03-04 13:54:25 +0530

    ok

--- src/main/java/ngo/nabarun/test/ngo_nabarun_test/repository/UserDAO.java
+++ src/main/java/ngo/nabarun/test/ngo_nabarun_test/repository/UserDAO.java
@@ -13,6 +13,6 @@ public interface UserDAO {
     @SqlQuery("SELECT * FROM \"user_profiles\" WHERE \"firstName\" = :firstName AND \"lastName\" = :lastName LIMIT 1")
     UserDBModel findByName(@Bind("firstName") String firstName, @Bind("lastName") String lastName);
 
-    @SqlQuery("SELECT * FROM \"user_profiles\" WHERE \"roles\" = :roleCode")
+    @SqlQuery("SELECT * FROM \"user_profiles\" inner join user_roles on user_profiles.id = user_roles.userId WHERE user_roles.roleCode = :roleCode")
     List<UserDBModel> findByRole(@Bind("roleCode") String roleCode);
 }
